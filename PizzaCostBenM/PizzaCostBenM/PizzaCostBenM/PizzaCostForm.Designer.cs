832d25f994958386e2ba5d8cefa2b1eb64b3adf6
benmotz2002 <43444941+benmotz2002@users.noreply.github.com>
2018-10-09 09:59:27 -0400

    Updated Pizza Cost

--- PizzaCostBenM/PizzaCostBenM/PizzaCostBenM/PizzaCostForm.Designer.cs
+++ PizzaCostBenM/PizzaCostBenM/PizzaCostBenM/PizzaCostForm.Designer.cs
@@ -30,8 +30,17 @@
         {
             this.lbldiameterpizza = new System.Windows.Forms.Label();
             this.lblnumberpizza = new System.Windows.Forms.Label();
-            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
-            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.nudDiameter = new System.Windows.Forms.NumericUpDown();
+            this.nudNumberOfZa = new System.Windows.Forms.NumericUpDown();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.lblYourSubIs = new System.Windows.Forms.Label();
+            this.lblYourHstIs = new System.Windows.Forms.Label();
+            this.lblYourTotIs = new System.Windows.Forms.Label();
+            this.lblSubtotal = new System.Windows.Forms.Label();
+            this.lblHst = new System.Windows.Forms.Label();
+            this.lblTotal = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.nudDiameter)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudNumberOfZa)).BeginInit();
             this.SuspendLayout();
             // 
             // lbldiameterpizza
@@ -54,40 +63,143 @@
             this.lblnumberpizza.TabIndex = 1;
             this.lblnumberpizza.Text = "How many pizzas did you buy?";
             // 
-            // numericUpDown1
+            // nudDiameter
             // 
-            this.numericUpDown1.Location = new System.Drawing.Point(403, 54);
-            this.numericUpDown1.Maximum = new decimal(new int[] {
+            this.nudDiameter.Location = new System.Drawing.Point(403, 54);
+            this.nudDiameter.Maximum = new decimal(new int[] {
             20,
             0,
             0,
             0});
-            this.numericUpDown1.Minimum = new decimal(new int[] {
+            this.nudDiameter.Minimum = new decimal(new int[] {
             3,
             0,
             0,
             0});
-            this.numericUpDown1.Name = "numericUpDown1";
-            this.numericUpDown1.Size = new System.Drawing.Size(120, 20);
-            this.numericUpDown1.TabIndex = 2;
-            this.numericUpDown1.Value = new decimal(new int[] {
-            20,
+            this.nudDiameter.Name = "nudDiameter";
+            this.nudDiameter.Size = new System.Drawing.Size(120, 20);
+            this.nudDiameter.TabIndex = 2;
+            this.nudDiameter.Value = new decimal(new int[] {
+            3,
             0,
             0,
             0});
-            this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            this.nudDiameter.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // nudNumberOfZa
+            // 
+            this.nudNumberOfZa.Location = new System.Drawing.Point(403, 105);
+            this.nudNumberOfZa.Maximum = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.nudNumberOfZa.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.nudNumberOfZa.Name = "nudNumberOfZa";
+            this.nudNumberOfZa.Size = new System.Drawing.Size(120, 20);
+            this.nudNumberOfZa.TabIndex = 3;
+            this.nudNumberOfZa.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCalculate.Location = new System.Drawing.Point(244, 190);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(134, 51);
+            this.btnCalculate.TabIndex = 4;
+            this.btnCalculate.Text = "Calculate";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // lblYourSubIs
+            // 
+            this.lblYourSubIs.AutoSize = true;
+            this.lblYourSubIs.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblYourSubIs.Location = new System.Drawing.Point(68, 292);
+            this.lblYourSubIs.Name = "lblYourSubIs";
+            this.lblYourSubIs.Size = new System.Drawing.Size(126, 20);
+            this.lblYourSubIs.TabIndex = 5;
+            this.lblYourSubIs.Text = "Your Subtotal is:";
+            // 
+            // lblYourHstIs
+            // 
+            this.lblYourHstIs.AutoSize = true;
+            this.lblYourHstIs.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblYourHstIs.Location = new System.Drawing.Point(68, 334);
+            this.lblYourHstIs.Name = "lblYourHstIs";
+            this.lblYourHstIs.Size = new System.Drawing.Size(91, 20);
+            this.lblYourHstIs.TabIndex = 6;
+            this.lblYourHstIs.Text = "The HST is:";
+            // 
+            // lblYourTotIs
+            // 
+            this.lblYourTotIs.AutoSize = true;
+            this.lblYourTotIs.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblYourTotIs.Location = new System.Drawing.Point(68, 381);
+            this.lblYourTotIs.Name = "lblYourTotIs";
+            this.lblYourTotIs.Size = new System.Drawing.Size(101, 20);
+            this.lblYourTotIs.TabIndex = 7;
+            this.lblYourTotIs.Text = "Your Total is:";
+            // 
+            // lblSubtotal
+            // 
+            this.lblSubtotal.AutoSize = true;
+            this.lblSubtotal.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSubtotal.Location = new System.Drawing.Point(200, 292);
+            this.lblSubtotal.Name = "lblSubtotal";
+            this.lblSubtotal.Size = new System.Drawing.Size(42, 20);
+            this.lblSubtotal.TabIndex = 8;
+            this.lblSubtotal.Text = "XXX";
+            // 
+            // lblHst
+            // 
+            this.lblHst.AutoSize = true;
+            this.lblHst.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblHst.Location = new System.Drawing.Point(165, 334);
+            this.lblHst.Name = "lblHst";
+            this.lblHst.Size = new System.Drawing.Size(42, 20);
+            this.lblHst.TabIndex = 9;
+            this.lblHst.Text = "XXX";
+            // 
+            // lblTotal
+            // 
+            this.lblTotal.AutoSize = true;
+            this.lblTotal.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTotal.Location = new System.Drawing.Point(177, 381);
+            this.lblTotal.Name = "lblTotal";
+            this.lblTotal.Size = new System.Drawing.Size(42, 20);
+            this.lblTotal.TabIndex = 10;
+            this.lblTotal.Text = "XXX";
             // 
             // frmPizzaCost
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(627, 461);
-            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.lblTotal);
+            this.Controls.Add(this.lblHst);
+            this.Controls.Add(this.lblSubtotal);
+            this.Controls.Add(this.lblYourTotIs);
+            this.Controls.Add(this.lblYourHstIs);
+            this.Controls.Add(this.lblYourSubIs);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.nudNumberOfZa);
+            this.Controls.Add(this.nudDiameter);
             this.Controls.Add(this.lblnumberpizza);
             this.Controls.Add(this.lbldiameterpizza);
             this.Name = "frmPizzaCost";
             this.Text = "Pizza Cost";
-            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.Load += new System.EventHandler(this.frmPizzaCost_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.nudDiameter)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudNumberOfZa)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -97,7 +209,15 @@
 
         private System.Windows.Forms.Label lbldiameterpizza;
         private System.Windows.Forms.Label lblnumberpizza;
-        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.NumericUpDown nudDiameter;
+        private System.Windows.Forms.NumericUpDown nudNumberOfZa;
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.Label lblYourSubIs;
+        private System.Windows.Forms.Label lblYourHstIs;
+        private System.Windows.Forms.Label lblYourTotIs;
+        private System.Windows.Forms.Label lblSubtotal;
+        private System.Windows.Forms.Label lblHst;
+        private System.Windows.Forms.Label lblTotal;
     }
 }
 
--- PizzaCostBenM/PizzaCostBenM/PizzaCostBenM/PizzaCostForm.cs
+++ PizzaCostBenM/PizzaCostBenM/PizzaCostBenM/PizzaCostForm.cs
@@ -1,4 +1,11 @@
-﻿using System;
+﻿/*
+ * Created by: Ben Motz
+ * Created on: 01-Oct-2018
+ * Created for: ICS3U Programming
+ * Daily Assignment – Day #12 - Pseudocode
+ * This program displays the subtotal ,HST and total of a pizza depedning on diameter and number of pizzas
+*/
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -20,6 +27,47 @@ namespace PizzaCostBenM
         private void numericUpDown1_ValueChanged(object sender, EventArgs e)
         {
 
+        }
+
+        private void frmPizzaCost_Load(object sender, EventArgs e)
+        {
+            //Hides Labels
+            this.lblSubtotal.Hide();
+            this.lblTotal.Hide();
+            this.lblHst.Hide();
+            this.lblYourHstIs.Hide();
+            this.lblYourSubIs.Hide();
+            this.lblYourTotIs.Hide();
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            //Declare Local Variables
+            double diameter, numberofza, subtotal, hstprice, total;
+
+            //Declare constant
+            const double HST = 0.13;
+
+            //Convert to double
+            diameter = (double)(nudDiameter.Value);
+            numberofza = (double)(nudNumberOfZa.Value);
+
+            //Calculate
+            subtotal = numberofza * (0.99+0.75+(diameter * 0.50));
+            hstprice = subtotal * HST;
+            total = hstprice + subtotal;
+
+            //Displays the cost
+            this.lblSubtotal.Text = String.Format("${0:0.00}", subtotal);
+            this.lblHst.Text = String.Format("${0:0.00}", hstprice);
+            this.lblTotal.Text = String.Format("${0:0.00}", total);
+
+            this.lblHst.Show();
+            this.lblSubtotal.Show();
+            this.lblTotal.Show();
+            this.lblYourHstIs.Show();
+            this.lblYourSubIs.Show();
+            this.lblYourTotIs.Show();
         }
     }
 }
